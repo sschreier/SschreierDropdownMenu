793f7519e4df36c0523608bfaebf53292b447a3d
Sebastian Schreier <info@sebastianschreier.de>
2023-05-01 17:59:27 +0200

    refactored code to convert plugin config values to SCSS variables

--- src/Subscriber/ThemeVariablesSubscriber.php
+++ src/Subscriber/ThemeVariablesSubscriber.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 namespace Sschreier\DropdownMenu\Subscriber;
 
 use Shopware\Core\System\SystemConfig\SystemConfigService;
-use Shopware\Storefront\Event\ThemeCompilerEnrichScssVariablesEvent;
+use Shopware\Storefront\Theme\Event\ThemeCompilerEnrichScssVariablesEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Sschreier\DropdownMenu\SschreierDropdownMenu;
 
 class ThemeVariablesSubscriber implements EventSubscriberInterface
 {
     private SystemConfigService $systemConfig;
 
-    private string $pluginDomain;
-
-    public function __construct(SystemConfigService $systemConfig, string $pluginDomain)
+    public function __construct(SystemConfigService $systemConfig)
     {
         $this->systemConfig = $systemConfig;
-        $this->pluginDomain = $pluginDomain;
     }
 
     public static function getSubscribedEvents(): array
@@ -29,21 +27,16 @@ class ThemeVariablesSubscriber implements EventSubscriberInterface
 
     public function onAddVariables(ThemeCompilerEnrichScssVariablesEvent $event): void
     {
-        $salesChannelId = $event->getSalesChannelId();
+        $configFields = $this->systemConfig->get('SschreierDropdownMenu.config', $event->getSalesChannelId());
 
-        $pluginConfig = $this->systemConfig
-            ->getDomain($this->pluginDomain, $salesChannelId, true);
+        foreach($configFields as $configKey => $value) {
+            // convert `customVariableName` to `custom-variable-name`
+            $kebabCasedScssVariable = str_replace('_', '-', (new CamelCaseToSnakeCaseNameConverter())->normalize($configKey));
 
-        if (empty($pluginConfig)) {
-            return;
-        }
-
-        foreach (SschreierDropdownMenu::PLUGIN_VARS as $configKey => $scssVariable) {
-            $config = $pluginConfig[$this->pluginDomain . $configKey] ?? null;
-            if ($config) {
+            if($value && in_array($configKey, SschreierDropdownMenu::PLUGIN_CONFIG_VARS)) {
                 $event->addVariable(
-                    $scssVariable,
-                    (string) $config
+                    'sschreier-' . $kebabCasedScssVariable,
+                    (string) $value
                 );
             }
         }
